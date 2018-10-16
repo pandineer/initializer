76a1d0b59b51f85904dcb184f528630b9bea7bc4
pandineer <pandineer@gmail.com>
2018-10-17 08:09:12 +0900

    initial commit

---
+++ centos.sh
@@ -0,0 +1,12 @@
+sudo yum update -y
+sudo yum groupinstall 'Development tools' -y
+sudo yum install emacs -y
+sudo yum install git -y
+sudo yum install screen -y
+sudo yum install go -y
+sudo yum install zsh -y
+
+sudo timedatectl set-timezone Asia/Tokyo
+sudo yum install chrony.x86_64 -y
+sudo systemctl start chronyd
+sudo chronyc -a makestep
